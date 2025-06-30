2d80df6c1cd0cf58621fc0f61b6e4094e80a2bfc
Pierre-Luc Charette <pierre-luc.charette@vooban.com>
2025-06-30 12:04:28 -0400

    Try deploy with 404

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // Set base path for GitHub Pages - replace 'cirque-rummy' with your actual repo name
+  // Set base path for GitHub Pages
   base: process.env.NODE_ENV === 'production' ? '/cirque-rummy/' : '/',
   server: {
     host: "::",
